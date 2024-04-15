0dff48679ecde67aaa2f50410b3fb2931e2ff79f
Laura <268312@student.pwr.edu.pl>
2024-04-15 11:51:38 +0200

    updated CalendarActivity

---
+++ app/src/main/java/com/example/projektzpo/CalendarActivity.kt
@@ -0,0 +1,24 @@
+package com.example.projektzpo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import com.example.projektzpo.databinding.FragmentCalendarBinding
+
+open class CalendarActivity : AppCompatActivity() {
+
+    private lateinit var binding: FragmentCalendarBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = FragmentCalendarBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calendarBack.setOnClickListener{
+            findNavController().navigate(R.id.action_CalendarFragment_to_SecondFragment)
+        }
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+}
