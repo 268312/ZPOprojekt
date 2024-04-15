20e95ec650764ce616b043bfa8bc87a021edea16
Laura <268312@student.pwr.edu.pl>
2024-04-15 12:17:26 +0200

    updated AnalysisActivity

---
+++ app/src/main/java/com/example/projektzpo/AnalysisActivity.kt
@@ -0,0 +1,12 @@
+package com.example.projektzpo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+open class AnalysisActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_analysis)
+    }
+}
